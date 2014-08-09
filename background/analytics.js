bf071a602f6abf602d61b812a3d1a1da3ec57589
Devin Rhode <devinrhode2@gmail.com>
2014-08-08 19:31:06 -0700

    Use real analytics.js instead

--- background/analytics.js
+++
@@ -1,6 +0,0 @@
-//Use real analytics.js instead
-//if necessary stub methods
-/*
-function globalFunctionStubbingAnalyticsDotjs
-window.globalFunctionStubbingAnalytics = globalFunctionStubbingAnalytics;
-*/
