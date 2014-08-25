6ad69dad7998ae81517a2d985051e8a5bc3820fd
Devin Rhode <devinrhode2@gmail.com>
2014-08-25 10:54:39 -0700

    loading mobile sites! using iPhone UA for looks

--- background.js
+++ background.js
@@ -1,59 +1,56 @@
 console.log('Scout started');
 
-if (NOT_INSTALLED) {
-  SET_INSTALLED_TRUE
-  chrome.tabs.create({
-    'url': chrome.extension.getURL('welcome/index.html')
-  });
-  //POST('http://thescoutapp.com/smartframes/post.php', 'install=occured', function emptyPostCallbackForInstallOccured(){});
-}
+// if (NOT_INSTALLED) {
+//   SET_INSTALLED_TRUE
+//   chrome.tabs.create({
+//     'url': chrome.extension.getURL('welcome/index.html')
+//   });
+//   //POST('http://thescoutapp.com/smartframes/post.php', 'install=occured', function emptyPostCallbackForInstallOccured(){});
+// }
+
+// var defaults = {width:22, on: 'yes'}; //only write if they are undefined. 
+// for (var item in defaults) {
+//   if (defaults.hasOwnProperty(item)) {
+//     if (CHROME_STORAGE.getItem(item) is null?) {
+//       CHROME_STORAGE.setItem(item, defaults[item]);
+//     }
+//   }
+// }
+
+// function set(setting, value) {
+//   // if setting has changed
+//   if (CHROME_STORAGE.getItem(setting).toString() !== value.toString()) {
+//     if (setting !== 'sites') {
+//       set.lastActionNote = 'localStorage['+setting+'] changed from: ' + CHROME_STORAGE.getItem(setting);
+//     }
+//     CHROME_STORAGE.setItem(setting, value);
+//   }
+// }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   //turn off or turn on, then reload tab
   console.log('browser action clicked. tab:', tab);
 });
 
-var defaults = {width:22, on: 'yes'}; //only write if they are undefined. 
-for (var item in defaults) {
-  if (defaults.hasOwnProperty(item)) {
-    if (CHROME_STORAGE.getItem(item) is null?) {
-      CHROME_STORAGE.setItem(item, defaults[item]);
-    }
-  }
-}
-
-chrome.webRequest.onBeforeSendHeaders.addListener(function(){
-  console.log('beforeSendHeaders', arguments);
-}/*, filter, opt_extraInfoSpec*/);
-
-
-function set(setting, value) {
-  // if setting has changed
-  if (CHROME_STORAGE.getItem(setting).toString() !== value.toString()) {
-    if (setting !== 'sites') {
-      set.lastActionNote = 'localStorage['+setting+'] changed from: ' + CHROME_STORAGE.getItem(setting);
-    }
-    CHROME_STORAGE.setItem(setting, value);
-  }
-}
-
-//Allow iframes to load from any site, remote x-frame-options restrictions
+//Allow iframes to load from any site, remove x-frame-options restrictions
 chrome.webRequest.onHeadersReceived.addListener(
-  function onHeadersReceivedListener(resp) {
-    resp.responseHeaders.forEach(function forEachResponseHeader(header, index) {
-      header = header.name.toLowerCase();
-      if (header === 'x-frame-options')
-        resp.responseHeaders.splice(index,1);
-      if (header === 'access-control-allow-origin')
-        resp.responseHeaders[index].value='*';
-      if (header === 'x-framework-css-blocks')
+  function (resp) {
+    resp.responseHeaders.forEach(function(header, index) {
+      header = header.name.toLowerCase()
+
+             if (header === 'x-frame-options') {
+        resp.responseHeaders.splice(index,1)
+      } else if (header === 'access-control-allow-origin') {
+        resp.responseHeaders[index].value = '*'
+      } else if (header === 'x-framework-css-blocks') {
         console.log(resp.responseHeaders[index].value)
       }
-    );
+
+    })
     resp.responseHeaders[resp.responseHeaders.length] = {
-      'name':'Access-Control-Allow-Origin',
-      'value':'*'
-    };
+      'name': 'Access-Control-Allow-Origin',
+      'value': '*'
+    }
     //WHY RETURN NEW OBJECT VS resp??
     return {responseHeaders:resp.responseHeaders}
   }, //end onHeadersRecievedListener
@@ -62,7 +59,37 @@ chrome.webRequest.onHeadersReceived.addListener(
     types:['sub_frame']
   },
   ['blocking','responseHeaders']
-);
+)
+
+/*
+Desktop:
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4)            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36
+
+Mobile:
+Mozilla/5.0 (Linux; Android 4.4.4; SGH-M919 Build/KTU84Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.135 Mobile Safari/537.36
+*/
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function (req) {
+    console.log('onBeforeSendHeaders');
+    req.requestHeaders.forEach(function(header, index){
+      console.log(header.name, header.value);
+      if (header.name === 'User-Agent') {
+        if (header.value.indexOf('Mobile Safari') === -1) {
+          req.requestHeaders[index].value = 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D201';
+          console.log('fixed?', req.requestHeaders[index].value);
+        }
+      }
+    })
+    return {requestHeaders: req.requestHeaders};
+  },
+  {
+    urls: ['<all_urls>'],
+    types:['main_frame']
+  },
+  ['blocking', 'requestHeaders']
+)
+
 
 // function sendData(port) {
 //   var settings = {};
